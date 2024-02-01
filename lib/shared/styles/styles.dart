3070a9f975506168f3340db5007ca9630e59a622
Muhammad Rashad <admoali123@gmail.com>
2024-02-01 08:45:05 +0200

    add font and Style

---
+++ lib/shared/styles/styles.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const textb18=TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize:18,
+    
+  );
+  static const textbI18=  TextStyle(
+    fontWeight: FontWeight.bold,
+    fontStyle: FontStyle.italic,
+    fontSize:18,
+  );
+  static const text14= TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize:14,
+  );
+  
+  static const textb20= TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize:20,
+  );
+  static const textb30= TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize:30,
+  );
+
+  
+}
