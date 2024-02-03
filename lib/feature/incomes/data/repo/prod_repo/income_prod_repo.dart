54903366cffd20e26b8776f3b29f84ea915c8cba
Muhammad Rashad <admoali123@gmail.com>
2024-02-03 22:25:09 +0200

    implement del fun

--- lib/feature/incomes/data/repo/prod_repo/income_prod_repo.dart
+++ lib/feature/incomes/data/repo/prod_repo/income_prod_repo.dart
@@ -1,12 +1,10 @@
- 
- import 'package:injectable/injectable.dart';
+import 'package:injectable/injectable.dart';
 
 import '../../../../../injection.dart';
 import '../../../../../shared/data/app_db.dart';
 import '../../../domain/entity/income.dart';
 import '../../../domain/repo/icome_repo.dart';
 
- 
 // Injectable IncomeabReo
 @Injectable(as: IncomeabReo)
 // @Singleton(as:IncomeabReo,env: ["prod"])
@@ -14,25 +12,28 @@ import '../../../domain/repo/icome_repo.dart';
 class IncomeReo extends IncomeabReo {
   @override
   Future<List<Income>> allIncomes() async {
-    var x= getIt.get<AppDb>();
-    var l= await x.readdate(''' 
+    var x = getIt.get<AppDb>();
+    var l = await x.readdate(''' 
      SELECT *FROM "incomes"
     ''');
     return l.map((e) => Income.fromMap(e)).toList();
   }
 
   @override
-  Future<int> del(Income income) {
-    throw UnimplementedError();
+  Future<int> del(Income income) async {
+    AppDb appDb = getIt.get<AppDb>();
+    int re =
+        await appDb.detletitem('DELETE FROM incomes WHERE id = ${income.id}');
+    return re;
   }
 
   @override
   Future<int> insert(Income income) async {
-       var x= getIt.get<AppDb>();
-       return await x.insert('''  
+    var x = getIt.get<AppDb>();
+    return await x.insert('''  
        INSERT INTO Incomes(value, source, date)VALUES (${income.value},"${income.source}", ${income.date.millisecondsSinceEpoch})
        ''');
-   }
+  }
 
   @override
   Future<int> update(Income income) {
