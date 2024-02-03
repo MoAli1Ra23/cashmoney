2022f57c6a8b4154c9457dd25da57ea95f436d59
Muhammad Rashad <admoali123@gmail.com>
2024-02-03 11:57:46 +0200

    retouch

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -43,11 +44,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        fontFamily: 'Cairo',
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        scaffoldBackgroundColor: Colors.blueGrey.shade50
-      ),
+          fontFamily: 'Cairo',
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          scaffoldBackgroundColor: Colors.blueGrey.shade50),
       // home: const ManageIcomesPage(),
       home: const MyMain(),
       // const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -62,102 +62,139 @@ class MyMain extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.blueGrey.shade50,
+          backgroundColor: Colors.blueGrey.shade50,
           body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 50),
-            margin: const EdgeInsets.symmetric(horizontal: 50),
-            height: 80,
-            decoration: BoxDecoration(
-              color:   Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.blueGrey.shade700,
-                    offset: const Offset(0, 6),
-                    blurRadius: 10,
-                    spreadRadius: -5,
-                    blurStyle: BlurStyle.outer),
-              ],
-            ),
-            child: Row(
-              children: [
-                Consumer<WallateViewModel>(
-                  builder: (context, pt, child) {
-                     return Text(
-                      "الرصيد=${pt.text}",
-                      style: const TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueGrey),
-                                       );
-                  },
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                margin: const EdgeInsets.symmetric(horizontal: 50),
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.blueGrey.shade700,
+                        offset: const Offset(0, 6),
+                        blurRadius: 10,
+                        spreadRadius: -5,
+                        blurStyle: BlurStyle.outer),
+                  ],
                 ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                OutlinedButton(
-                  
-                  style: OutlinedButton.styleFrom(
-                    backgroundColor: Colors.blue.shade400,
-                    shadowColor: Colors.amber,
-                    
-                     shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadiusDirectional.only(
-          
-                        topStart: Radius.circular(10),
-                        bottomStart: Radius.circular(10),
-          
-                      ),
-                      
-                    )),
-                  onPressed: () {
-                    // context.read<ManageIcomes>().income();
-          
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (_) {
-                        return const ManageIcomesPage();
-                      }),
-                    );
-                  },
-                  child: const Center(
-                    child: Text(
-                      "الدخل",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25,
-                          color: Color.fromARGB(184, 242, 238, 238)),
+                child: Row(
+                  children: [
+                    Consumer<WallateViewModel>(
+                      builder: (context, pt, child) {
+                        return Text(
+                          "الرصيد=${pt.text}",
+                          style: const TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueGrey),
+                        );
+                      },
                     ),
-                  ),
+                  ],
                 ),
-                OutlinedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) => const ManageExpensesPage(),
-                    ));
-                  },
-                  child: const Center(
-                    child: Text(
-                      "النفقات",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
+              ),
+              SizedBox(
+                height: 60,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          backgroundColor: Colors.blue.shade400,
+                           shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadiusDirectional.only(
+                              topStart: Radius.circular(10),
+                              bottomStart: Radius.circular(10),
+                            ),
+                          )),
+                      onPressed: () {
+                        // context.read<ManageIcomes>().income();
+
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (_) {
+                            return const ManageIcomesPage();
+                          }),
+                        );
+                      },
+                      child: const Center(
+                        child: Text(
+                          "الدخل",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              color: Color.fromARGB(184, 242, 238, 238)),
+                        ),
                       ),
                     ),
-                  ),
+                    CustomButton(
+                      onPressed: () {},
+                      text: "aaa",
+                    ),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (_) => const ManageExpensesPage(),
+                        ));
+                      },
+                      child: const Center(
+                        child: Text(
+                          "النفقات",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ],
-      )),
+              ),
+            ],
+          )),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    Key? key,
+    this.onPressed,
+    required this.text,
+  }) : super(key: key);
+  final void Function()? onPressed;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.zero,
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(offset: Offset(2, 5), color: Colors.blueGrey.shade700)
+      ]),
+      child: OutlinedButton(
+        onPressed: onPressed,
+        // style: OutlinedButton.styleFrom(
+        //   backgroundColor: Colors.red,
+        //   shadowColor: Colors.green,
+        //   shape: const RoundedRectangleBorder(
+        //     borderRadius: BorderRadius.only(
+        //       topLeft: Radius.circular(
+        //         15,
+        //       ),
+        //       bottomRight: Radius.circular(
+        //         15,
+        //       ),
+        //     ),
+        //   ),
+        // )
+        
+        child: Text(text),
+      ),
     );
   }
 }
