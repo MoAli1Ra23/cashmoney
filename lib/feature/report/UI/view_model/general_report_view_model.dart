e5e7d590b1dded5823eb8b5700fde328f5c210e7
Muhammad Rashad <admoali123@gmail.com>
2024-02-05 10:24:20 +0200

    add validation

--- lib/feature/report/UI/view_model/general_report_view_model.dart
+++ lib/feature/report/UI/view_model/general_report_view_model.dart
@@ -2,29 +2,65 @@ import 'package:cashmoney/feature/report/domain/entity/general_report.dart';
 import 'package:cashmoney/injection.dart';
 import 'package:flutter/material.dart';
 
-import '../../data/repo/general_report_repo_impl.dart';
+import '../../domain/repo/general_report_repo.dart';
 
 class GeneralReportViewModel extends ChangeNotifier {
   bool isLoaded = false;
   late GeneralReport report;
-  late DateTime? begin;
-  late DateTime? end;
-   void setBegin(DateTime dateTime)
-   {
-    begin=dateTime;
+  late DateTime? begin = DateTime.now();
+  late DateTime? end = DateTime.now();
+  String? validateBegin;
+  String? validateEnd;
 
-   }
-   void setEnd(DateTime dateTime)
-   {
-    end=dateTime;
+  void setBegin({DateTime? dateTime, String? text}) {
+    if (text != null) validateBeginText(text);
+    if (dateTime != null && validateBegin == null) begin = dateTime;
+  }
 
-   }
-  Future<void> generateReport() async {
-    GeneralReportRepoImpl repo = getIt.get<GeneralReportRepoImpl>();
-    if (begin != null && end != null) {
-    report= await  repo.generatReport(begin!, end!);
-    isLoaded=true;
-    notifyListeners();
+  void validateBeginText(String text) {
+    var v = validateDate(text);
+
+    if (v == null) {
+      begin == DateTime.parse(text);
+    } else {
+      validateBegin = v;
+
+      notifyListeners();
     }
+  }
+
+  void validateEndText(String text) {
+    var v = validateDate(text);
+
+    if (v == null) {
+      end == DateTime.parse(text);
+    } else {
+      validateEnd = v;
+
+      notifyListeners();
+    }
+  }
+
+  void setEnd({DateTime? dateTime, String? text}) {
+    if (text != null) validateEndText(text);
+
+    if (dateTime != null && validateEnd == null) end = dateTime;
+  }
+
+  Future<void> generateReport() async {
+    GeneralReportRepo repo = getIt.get<GeneralReportRepo>();
+    if (begin != null && end != null) {
+      report = await repo.generatReport(begin!, end!);
+      isLoaded = true;
+      notifyListeners();
+    }
+  }
+
+  String? validateDate(String dateTime) {
+    var v = DateTime.tryParse(dateTime);
+    if (v == null) {
+      return "format error";
+    }
+    return null;
   }
 }
